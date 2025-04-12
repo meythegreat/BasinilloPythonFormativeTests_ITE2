d6ac3371c4b97c9c2d81d434be3d874b1fb9803d
Miguel Angelo Basinillo <miguel102104@gmail.com>
2025-04-12 12:47:13 +0800

    Updated answers for Module 1 and added Module 4.3

--- Module 1/module_1.py
+++ Module 1/module_1.py
@@ -3,10 +3,10 @@
 # MODULE 1 - Formative Test
 
 # 1. "What are the primary features of Python that make it a preferred language for both novice and experienced programmers?"
-#     Python is preferred because it is easy to learn, highly readable, versatile, and supports multiple programming paradigms such as object-oriented, procedural, and functional programming. It has a large standard library and strong community support.
+#     Python is preferred due to the fact that it is simple to learn, readable, versatile, and has multi-paradigm support for object-oriented, procedural, and functional programming. Python has a wide range of library support and strong community.
 
 # 2.  "How does Python's readability and simplicity contribute to its widespread adoption in different technological fields, according to the introduction?"
-#     Python’s readability and simplicity allow developers to write clean and understandable code, reducing development time and minimizing errors. This makes Python suitable for use in education, web development, data science, artificial intelligence, and many other fields.
+#     Python’s readability and ease of use enable programmers to write clean and clear code, which cuts down on development time and minimizes bugs. It makes Python ideal for educational purposes, web application development, data science, and artificial intelligence, among numerous other applications.
 
 # 3. "In what ways does the introduction explain Python's role in facilitating tasks such as web development, data analysis, and machine learning?"
-#     The introduction explains that Python offers powerful libraries and frameworks like Django for web development, Pandas and NumPy for data analysis, and TensorFlow and Scikit-learn for machine learning, making complex tasks easier and more efficient to perform.
+#     The introduction mentions that Python provides strong libraries such as Django for web development, Pandas and NumPy for data science, and machine learning through libraries like TensorFlow and Scikit-learn, making complicated operations simpler and faster to accomplish.
---
+++ Module 4/module_4.3.py
@@ -0,0 +1,10 @@
+# Name: Miguel Angelo B. Basinillo
+# Section: BSIT 2A
+# MODULE 4.3 - Formative Test
+# 3. Develop a program that determines if a given year is a leap year or not and prints the result.
+
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
